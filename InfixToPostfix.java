c746a59611a7516cbf88af789f91305f0fa575e3
Sasi Kumar T <32474530+Sasikumar3096@users.noreply.github.com>
2019-07-19 16:13:35 +0530

    Update InfixToPostfix.java

--- InfixToPostfix.java
+++ InfixToPostfix.java
@@ -14,7 +14,7 @@ public class InfixToPostfix {
 	return -1;
 	}
 	public static void main(String[] args) {
-		char exp[]= "(a+b)-c".toCharArray();
+		char exp[]= "A+(B*C-(D/E-F)*G)*H".toCharArray();
 		String result="";
 		Stack<Character> stack = new Stack<Character>(); 
 		int k=0;
@@ -30,10 +30,13 @@ public class InfixToPostfix {
 				else
 					if(exp[i]==')')
 					{
+						System.out.println(stack.size());
 						while(!stack.isEmpty() && stack.peek()!='('){
 							result += stack.pop();
-						stack.pop();
 						}
+						if(!stack.isEmpty())
+							stack.pop();
+						
 					}
 					else {
 						while(!stack.isEmpty() && prec(exp[i])<=prec(stack.peek()))
